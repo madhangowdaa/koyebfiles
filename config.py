07fb4fd034c4d7aed894092026fbf9e5426a80fb
Madhan H B <104442213+madhangowdaa@users.noreply.github.com>
2024-06-16 19:08:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -61,7 +61,7 @@ else:
     DISABLE_CHANNEL_BUTTON = False
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "<b> Hi {first} \n\n »»————>🄼🄰🄺<————««\n\n𝙏𝙧𝙮 --> /join"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
