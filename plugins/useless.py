6848ee1091d5f224d1044d7924555f7f200d4868
Madhan H B <104442213+madhangowdaa@users.noreply.github.com>
2025-01-23 19:41:59 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -15,7 +15,7 @@ async def followus(bot: Bot, message: Message):
                           InlineKeyboardButton('🎬 𝑴𝒐𝒗𝒊𝒆𝒔 𝒈𝒓𝒐𝒖𝒑', url="t.me/+UfmaZyLntv41Yzg1"),
                           InlineKeyboardButton('🥹 𝑼𝒑𝒅𝒂𝒕𝒆𝒔 𝑪𝒉𝒂𝒏𝒏𝒆𝒍', url="t.me/MoviezAddaKA")
                        ],[
-                          InlineKeyboardButton("🧑‍💻 𝑩𝒐𝒕 𝑪𝒓𝒆𝒂𝒕𝒆𝒓", url="t.me/Nimgeyakhe")
+                          InlineKeyboardButton("🧑‍💻 𝑩𝒐𝒕 𝑪𝒓𝒆𝒂𝒕𝒆𝒓", url="t.me/kmadminsbot")
                          ]
                         ]
                     )
